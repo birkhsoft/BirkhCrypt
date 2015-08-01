f3a1ae7142549de8f5c31756f9d73e19328693a2
BirkhSoft <birkhsoft@gmail.com>
2015-08-01 23:19:44 +0200

    Programa para encriptar con el algoritmo 3DES.

---
+++ BirkhCrypt/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btn_crypt = New System.Windows.Forms.Button()
+        Me.btn_decrypt = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(257, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(55, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(229, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 54)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(125, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'btn_crypt
+        '
+        Me.btn_crypt.Location = New System.Drawing.Point(13, 95)
+        Me.btn_crypt.Name = "btn_crypt"
+        Me.btn_crypt.Size = New System.Drawing.Size(103, 32)
+        Me.btn_crypt.TabIndex = 3
+        Me.btn_crypt.Text = "Encriptar"
+        Me.btn_crypt.UseVisualStyleBackColor = True
+        '
+        'btn_decrypt
+        '
+        Me.btn_decrypt.Location = New System.Drawing.Point(139, 95)
+        Me.btn_decrypt.Name = "btn_decrypt"
+        Me.btn_decrypt.Size = New System.Drawing.Size(103, 32)
+        Me.btn_decrypt.TabIndex = 4
+        Me.btn_decrypt.Text = "Desencriptar"
+        Me.btn_decrypt.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(328, 173)
+        Me.Controls.Add(Me.btn_decrypt)
+        Me.Controls.Add(Me.btn_crypt)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents btn_crypt As System.Windows.Forms.Button
+    Friend WithEvents btn_decrypt As System.Windows.Forms.Button
+
+End Class
---
+++ BirkhCrypt/Form1.vb
@@ -0,0 +1,107 @@
+﻿Imports System
+Imports System.IO
+Imports System.Security
+Imports System.Security.Cryptography
+Imports System.Text
+
+'
+' .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+'| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+'| |   ______     | || |     _____    | || |  _______     | || |  ___  ____   | || |  ____  ____  | || |     ____     | || |  _________   | || |  _________   | |
+'| |  |_   _ \    | || |    |_   _|   | || | |_   __ \    | || | |_  ||_  _|  | || | |_   ||   _| | || |   .'    '.   | || | |_   ___  |  | || | |_   ___  |  | |
+'| |    | |_) |   | || |      | |     | || |   | |__) |   | || |   | |_/ /    | || |   | |__| |   | || |  |  .--.  |  | || |   | |_  \_|  | || |   | |_  \_|  | |
+'| |    |  __'.   | || |      | |     | || |   |  __ /    | || |   |  __'.    | || |   |  __  |   | || |  | |    | |  | || |   |  _|      | || |   |  _|      | |
+'| |   _| |__) |  | || |     _| |_    | || |  _| |  \ \_  | || |  _| |  \ \_  | || |  _| |  | |_  | || |  |  `--'  |  | || |  _| |_       | || |  _| |_       | |
+'| |  |_______/   | || |    |_____|   | || | |____| |___| | || | |____||____| | || | |____||____| | || |   '.____.'   | || | |_____|      | || | |_____|      | |
+'| |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+'| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+''----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'
+
+
+
+Public Class Form1
+    Private Const sSecretKey As String = "Password"
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_crypt.Click
+        EncryptFile(TextBox1.Text,
+                        TextBox1.Text & ".crypt6",
+                        sSecretKey)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btn_decrypt.Click
+        DecryptFile(TextBox1.Text,
+                    TextBox1.Text & ".decrypt6",
+                    sSecretKey)
+    End Sub
+
+    Sub EncryptFile(ByVal sInputFilename As String, _
+                  ByVal sOutputFilename As String, _
+                  ByVal sKey As String)
+
+        Dim fsInput As New FileStream(sInputFilename, _
+                                    FileMode.Open, FileAccess.Read)
+        Dim fsEncrypted As New FileStream(sOutputFilename, _
+                                    FileMode.Create, FileAccess.Write)
+
+        Dim DES As New DESCryptoServiceProvider()
+
+        DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey)
+
+        DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey)
+
+        Dim desencrypt As ICryptoTransform = DES.CreateEncryptor()
+        Dim cryptostream As New CryptoStream(fsEncrypted,
+                                            desencrypt,
+                                            CryptoStreamMode.Write)
+
+        Dim bytearrayinput(fsInput.Length - 1) As Byte
+        fsInput.Read(bytearrayinput, 0, bytearrayinput.Length)
+        cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length)
+        cryptostream.Close()
+    End Sub
+
+    Sub DecryptFile(ByVal sInputFilename As String, _
+        ByVal sOutputFilename As String, _
+        ByVal sKey As String)
+
+        Dim DES As New DESCryptoServiceProvider()
+        DES.Key() = ASCIIEncoding.ASCII.GetBytes(sKey)
+        DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey)
+
+        Dim fsread As New FileStream(sInputFilename, FileMode.Open, FileAccess.Read)
+        Dim desdecrypt As ICryptoTransform = DES.CreateDecryptor()
+        Dim cryptostreamDecr As New CryptoStream(fsread, desdecrypt, CryptoStreamMode.Read)
+        Dim fsDecrypted As New StreamWriter(sOutputFilename)
+        fsDecrypted.Write(New StreamReader(cryptostreamDecr).ReadToEnd)
+        fsDecrypted.Flush()
+        fsDecrypted.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myStream As Stream = Nothing
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.InitialDirectory = "c:\"
+        openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+                myStream = openFileDialog1.OpenFile()
+                If (myStream IsNot Nothing) Then
+                End If
+                TextBox1.Text = openFileDialog1.FileName
+            Catch Ex As Exception
+                MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
+            Finally
+
+                If (myStream IsNot Nothing) Then
+                    myStream.Close()
+                End If
+            End Try
+        End If
+    End Sub
+
+
+End Class
